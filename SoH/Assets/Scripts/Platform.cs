0bd576dc36460dd15a8ecb7bf55a6172de206e56
HBerke123 <huseyinberkedenizz@gmail.com>
2024-01-16 23:16:03 +0300

    Time Changer on progress

---
+++ SoH/Assets/Scripts/Platform.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public float uplimit;
+    public float downlimit;
+    public float speed;
+    bool up = true;
+
+    private void FixedUpdate()
+    {
+        if ((this.transform.position.y < uplimit) && up) transform.position += Vector3.up * speed;
+        else up = false;
+        if ((this.transform.position.y > downlimit) && !up) transform.position += Vector3.down * speed;
+        else up = true;
+    }
+}
