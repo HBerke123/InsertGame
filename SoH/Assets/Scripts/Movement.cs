ef7fbcbdbc2a630e3471395185455bf9a5d16423
Legendary Hyper <141352658+LegendaryHyper@users.noreply.github.com>
2024-01-17 20:09:40 +0300

    idk

--- SoH/Assets/Scripts/Movement.cs
+++ SoH/Assets/Scripts/Movement.cs
@@ -11,6 +11,7 @@ public class Movement : MonoBehaviour
     Rigidbody2D rb;
     float timePressed = 0f;
     public float agilityFactor = 5;
+    float timePressedHolder = 0f;
     private void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
@@ -18,29 +19,54 @@ public class Movement : MonoBehaviour
 
     private void Update()
     {
+        
         if (moveable) rb.velocity = new Vector2(speed * Input.GetAxisRaw("Horizontal"), rb.velocity.y);
         else rb.velocity = new Vector2(0, rb.velocity.y);
 
         if (Input.GetKeyDown(KeyCode.Space) && grounded && rb.velocity.y <= jumpforce)
         {
-            timePressed = Time.time;
+            timePressedHolder = Time.time;
+            rb.AddForce(Vector2.up * jumpforce * agilityFactor / 4, ForceMode2D.Impulse);
             grounded = false;
         }
 
         if (Input.GetKeyUp(KeyCode.Space) && grounded && rb.velocity.y <= jumpforce)
         {
-            timePressed = Time.time - timePressed;
-            Debug.Log(timePressed);
-            if (timePressed < 3)
+
+
+            timePressed = Time.time - timePressedHolder;
+
+                Debug.Log(timePressed);
+            if (timePressed < 1 && timePressed != Time.time)
             {
+                if (Time.time - timePressed < 1)
+                {
+                    timePressed = Time.time - timePressed;
+                }
                 rb.AddForce(Vector2.up * jumpforce * agilityFactor / 4, ForceMode2D.Impulse);
                 grounded = false;
             }
-            else if (timePressed >= 3)
+
+            else if (timePressed >= 5 && timePressed != Time.time)
             {
-                rb.AddForce(Vector2.up * jumpforce * 3 * agilityFactor / 4, ForceMode2D.Impulse);
+
+                if (Time.time - timePressed < 1)
+                {
+                    timePressed = Time.time - timePressed;
+                }
+                rb.AddForce(Vector2.up * jumpforce * 3 * agilityFactor * Mathf.Sqrt(5) / 4, ForceMode2D.Impulse);
                 grounded = false;
             }
+            else if (timePressed != Time.time)
+            {
+                if (Time.time - timePressed < 1)
+                {
+                    timePressed = Time.time - timePressed;
+                }
+                rb.AddForce(Vector2.up * jumpforce * 3 * agilityFactor * Mathf.Sqrt(timePressed) / 4, ForceMode2D.Impulse);
+                grounded = false;
+            }
+            Debug.Log("Pressed for : " + timePressed + " Seconds");
         }
 
         Vector2 location = new Vector3(transform.position.x - 0.50f, transform.position.y - 0.75f, 0);
