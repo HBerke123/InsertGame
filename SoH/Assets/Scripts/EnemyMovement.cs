6da3489657cf9b895aa66825471f7a3c8855aee7
HBerke123 <huseyinberkedenizz@gmail.com>
2024-02-10 01:39:25 +0300

    Enemy AI in fix process

--- SoH/Assets/Scripts/EnemyMovement.cs
+++ SoH/Assets/Scripts/EnemyMovement.cs
@@ -14,21 +14,22 @@ public class EnemyMovement : MonoBehaviour
     private void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
+        foreach (GameObject gameObject in FindObjectsOfType<GameObject>()) {
+            if (gameObject.CompareTag("Player"))
+            {
+                player = gameObject;
+                break;
+            }
+        }
     }
 
     private void Update()
     {
         distance = Mathf.Sqrt(Mathf.Pow(Mathf.Abs(this.transform.position.x - player.transform.position.x), 2) + Mathf.Pow(Mathf.Abs(this.transform.position.y - player.transform.position.y), 2));
-        if (distance <= 4)
+        if (distance <= 8)
         {
-            if (player.transform.position.x < this.transform.position.x) rb.AddForce((speed - rb.velocity.x) * Vector2.right);
-            else if (player.transform.position.x > this.transform.position.x) rb.AddForce((speed + rb.velocity.x) * Vector2.left);
-
-            if ((player.transform.position.y > this.transform.position.y + jumpforce / 6.25f) && grounded)
-            {
-                rb.AddForce(Vector2.up * jumpforce, ForceMode2D.Impulse);
-                grounded = false;
-            }
+            if (player.transform.position.x < this.transform.position.x) rb.AddForce(-speed * Vector2.right);
+            else if (player.transform.position.x > this.transform.position.x) rb.AddForce(-speed * Vector2.left);
         }
 
         Vector2 location = new Vector3(transform.position.x - 0.50f, transform.position.y - 0.75f, 0);
