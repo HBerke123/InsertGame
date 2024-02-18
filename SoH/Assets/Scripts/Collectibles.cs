bea59440716f63e4038359aa7de382e2f9acac9b
Legendary Hyper <141352658+LegendaryHyper@users.noreply.github.com>
2024-02-18 23:53:33 +0300

    HP Orb in progress

---
+++ SoH/Assets/Scripts/Collectibles.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectibles : MonoBehaviour
+{
+    public GameObject healthBar;
+    public GameObject healthDisplay;
+    public Collider2D playerCollider;
+    public
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBar = GameObject.Find("HP Bar");
+        healthDisplay = GameObject.Find("HP Display");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (HealthDrainage.health >= HealthDrainage.maxHealth)
+        {
+            HealthDrainage.health = HealthDrainage.maxHealth;
+            UpdateHealthBar(HealthDrainage.health / HealthDrainage.maxHealth);
+        }
+        Debug.Log(HealthDrainage.health);
+        
+    }
+    public void Heal(float healed)
+    {
+        HealthDrainage.health += healed;
+        Mathf.Round(HealthDrainage.health);
+        UpdateHealthBar(HealthDrainage.health / HealthDrainage.maxHealth);
+    }
+    public static void UpdateHealthBar(float newHealth)
+    {
+        HealthDrainage.slider.value = newHealth;
+        HealthDrainage.hpDisplayText.text = HealthDrainage.health + "/" + Mathf.Round(HealthDrainage.maxHealth);
+    }
+    void OnCollisionEnter2D(Collision2D target)
+    {
+        if (target.gameObject.CompareTag("HP Orb"))
+        {
+        Heal(20.0f);
+        }
+    }
+}
