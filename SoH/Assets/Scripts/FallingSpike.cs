7ba4adcddef53e395f5460fcd300cd1284d78568
Nightingale <honeysever09@gmail.com>
2024-04-19 21:56:53 +0300

    FallingSpikes

---
+++ SoH/Assets/Scripts/FallingSpike.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingSpike : MonoBehaviour
+{
+    Rigidbody2D rb;
+    BoxCollider2D boxCollider2D;
+    public float distance;
+    bool isFalling = false;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Physics2D.queriesStartInColliders = false;
+        if(isFalling == false)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position,Vector2.down,distance);
+
+            Debug.DrawRay(transform.position,Vector2.down * distance, Color.red);
+
+            if(hit.transform != null)
+            {
+                if(hit.transform.tag == "Player")
+                {
+                    rb.gravityScale = 5;
+                    isFalling = true;
+                }
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.collider.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            rb.gravityScale = 0;
+            boxCollider2D.enabled = false;
+        }
+    }
+}
