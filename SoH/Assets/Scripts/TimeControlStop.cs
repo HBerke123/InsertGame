94f9f2a832129a4f67e08e2251d4d0d1a09e1b0a
Nightingale <honeysever09@gmail.com>
2024-04-21 18:21:12 +0300

    THE TİME HAS COME

---
+++ SoH/Assets/Scripts/TimeControlStop.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeControlStop : MonoBehaviour
+{
+   [Header("TimeControllerSettings")]
+   public float TimeScale;
+
+   private float StartTimeScale;
+   private float StartFixedDeltaTime;
+
+
+    void Start()
+    {
+      StartTimeScale = Time.timeScale;
+      StartFixedDeltaTime = Time.fixedDeltaTime;
+    }
+
+    
+    void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.Y))
+      {
+         StartSlowMotion();
+      }
+
+      if(Input.GetKeyUp(KeyCode.Y))
+      {
+         StopSlowMotion();
+      }
+    }
+
+   public void StartSlowMotion()
+   {
+      Time.timeScale = TimeScale;
+      Time.fixedDeltaTime = StartFixedDeltaTime * TimeScale;
+   }
+
+   public void StopSlowMotion()
+   {
+      Time.timeScale = StartTimeScale;
+      Time.fixedDeltaTime = StartFixedDeltaTime;
+   }
+}
