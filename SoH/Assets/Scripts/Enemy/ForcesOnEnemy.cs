83d704cdab9b7e1eacf827cfbef00277a9782e46
HBerke123 <huseyinberkedenizz@gmail.com>
2024-07-08 16:55:42 +0300

    Walkable enemy added

--- SoH/Assets/Scripts/Enemy/ForcesOnEnemy.cs
+++ SoH/Assets/Scripts/Enemy/ForcesOnEnemy.cs
@@ -9,25 +9,20 @@ public class ForcesOnEnemy : MonoBehaviour
 
     private void Update()
     {
-        if ((Force.x > 0) || (Force.y > 0))
-        {
-            Debug.Log(Force);
-        }
-
-        if (Force.x > 0.1)
+        if ((Force.x > 0.1) || ((Force.x < -0.1)))
         {
             Force.x -= resistance * Force.x;
         }
-        else if (Force.x < 0.1)
+        else if ((Force.x < 0.1) || (Force.x > -0.1))
         {
             Force.x = 0;
         }
 
-        if (Force.y > 0.1)
+        if ((Force.y > 0.1) || ((Force.y < -0.1)))
         {
             Force.y -= resistance * Force.y;
         }
-        else if (Force.y < 0.1)
+        else if ((Force.y < 0.1) || (Force.y > -0.1))
         {
             Force.y = 0;
         }
