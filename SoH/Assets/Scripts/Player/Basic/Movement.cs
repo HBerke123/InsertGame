3fbaa82d544c4d7b2152dc1692b62ed095e40460
HBerke123 <huseyinberkedenizz@gmail.com>
2024-07-10 20:57:38 +0300

    Walk particles added

--- SoH/Assets/Scripts/Player/Basic/Movement.cs
+++ SoH/Assets/Scripts/Player/Basic/Movement.cs
@@ -5,13 +5,18 @@ using System.IO;
 
 public class Movement : MonoBehaviour
 {
-    public Rigidbody2D rb;
+    public ParticleSystem groundParticles;
+    public ParticleSystem lastParticles;
     public BoxCollider2D Attackhbox;
     public bool dashing;
     public bool stick;
     public float speed = 5;
     public float sspeed = 5;
     public float dspeed;
+    public float particleFrequency;
+    Rigidbody2D rb;
+    bool spawnParticles;
+    float th;
 
     private void Start()
     {
@@ -20,23 +25,58 @@ public class Movement : MonoBehaviour
         this.transform.position = new Vector3(float.Parse(File.ReadAllText(path + File.ReadAllText(path + "GSave.txt").Split("\n")[0] + ".txt").Split("\n")[3].Split(" ")[0]), float.Parse(File.ReadAllText(path + File.ReadAllText(path + "GSave.txt").Split("\n")[0] + ".txt").Split("\n")[3].Split(" ")[1]), 0);
     }
 
+    private void FixedUpdate()
+    {
+        if (spawnParticles && (th == 0))
+        {
+            th = Time.time - particleFrequency;
+        }
+
+        if ((Time.time - th >= particleFrequency) && (spawnParticles))
+        {
+            if (lastParticles != null)
+            {
+                Destroy(lastParticles.gameObject);
+            }
+            
+            ParticleSystem particles = Instantiate(groundParticles, this.transform.position - new Vector3(0, 1, 0), new Quaternion(0, 0, 0, 0));
+            if (this.GetComponent<SpriteRenderer>().flipX)
+            {
+                particles.gameObject.transform.localScale = new Vector3(-1, 1, 1);
+            }
+            lastParticles = particles;
+            th = Time.time;
+        }
+    }
+
     private void Update()
     {
         if (!dashing && !stick && (this.GetComponent<ForcesOnObject>().Force == Vector2.zero))
         {
             rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, rb.velocity.y);
+            if (Input.GetAxisRaw("Horizontal") != 0)
+            {
+                spawnParticles = true;
+            }
+            else
+            {
+                spawnParticles = false;
+            }
         }
         else if (!stick && (this.GetComponent<ForcesOnObject>().Force == Vector2.zero))
         {
             rb.velocity = new Vector2(dspeed, rb.velocity.y);
+            spawnParticles = false;
         }
         else if (!stick)
         {
             rb.velocity = this.GetComponent<ForcesOnObject>().Force;
+            spawnParticles = false;
         }
         else
         {
             rb.velocity = new Vector2(0, 0);
+            spawnParticles = false;
         }
         
         if ((Input.GetAxisRaw("Horizontal") != 0) && !Attackhbox.enabled) this.GetComponent<SpriteRenderer>().flipX = Input.GetAxisRaw("Horizontal") != 1;
