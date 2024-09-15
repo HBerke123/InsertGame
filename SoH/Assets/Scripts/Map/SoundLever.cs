af21afe09a261cf53a4e5ff567944132fbca2473
HBerke123 <151957984+HBerke123@users.noreply.github.com>
2024-09-15 19:25:25 +0300

    Revert "Base Ramp"
    This reverts commit c4095336f37d0571112a5b5ab32ce9bf417d31b1, reversing
    changes made to 084b3e6e352f5b77fff5c5e0210627619ceb0013.

--- SoH/Assets/Scripts/Map/SoundLever.cs
+++ SoH/Assets/Scripts/Map/SoundLever.cs
@@ -1,32 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Mathematics;
 using UnityEngine;
 
 public class SoundLever : MonoBehaviour
 {
-    public GameObject platform;
-    public GameObject secondPlatform;
-    public GameObject thirdPlatform;
-    public int num;
-    public bool isOn;
-
-    private void Update()
-    {
-        if (num == 0)
-        {
-            if (isOn)
-            {
-                platform.SetActive(false);
-                secondPlatform.SetActive(false);
-                thirdPlatform.SetActive(false);
-            }
-        }
-        else if (isOn) 
-        {
-            platform.SetActive(false);
-        }
-    }
+    bool isOn;
 
     public void ChangeStatment()
     {
