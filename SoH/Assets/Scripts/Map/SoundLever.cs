f5cc7242900f3845499a23dc5489631389964913
HBerke123 <151957984+HBerke123@users.noreply.github.com>
2024-09-22 17:42:00 +0300

    ancient tomb

--- SoH/Assets/Scripts/Map/SoundLever.cs
+++ SoH/Assets/Scripts/Map/SoundLever.cs
@@ -1,10 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SoundLever : MonoBehaviour
 {
+    public GameObject platform;
+    public GameObject secondPlatform;
+    public int num;
     bool isOn;
+
+    private void Update()
+    {
+        if (num == 0)
+        {
+            if (isOn)
+            {
+                platform.SetActive(false);
+                secondPlatform.SetActive(false);
+            }
+        }
+    }
 
     public void ChangeStatment()
     {
